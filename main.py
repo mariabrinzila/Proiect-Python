4b7240352baefec6acf654a8aa8b2fb1b0b9b8f6
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2021-12-20 19:18:00 +0200

    MODIFY main and plot_map
    plot_map => change map appearance (tiles), location attribute when creating the map is the average of all the locations' longitudes and latitudes
    main => generate tweets with hashtag here, print tweets, generates latitudes and longitudes, averages for the folium map creation, map creation

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import search_tweets as st
 import plot_map as pm
 
-pm.plotMap("#covid19", 15)
+tweets = st.generateTweetsWithHashtag("#covid19", 15)
+pm.printTweets(tweets)
+
+latitudes, longitudes = pm.locationsLongLat(tweets)
+latitude, longitude = pm.generateLatLongLocations(latitudes, longitudes)
+print("Number of locations on the map: " + str(len(latitudes)))
+print("Latitude: " + str(latitude) + " and longitude: " + str(longitude))
+
+pm.plotMap(latitudes, longitudes, latitude, longitude)
--- plot_map.py
+++ plot_map.py
@@ -1,21 +1,39 @@
-import search_tweets as st
 import folium
 from geopy.geocoders import Nominatim
+from numpy import mean
 
 
-def createMapMarkers(plotted_map, data):
+def locationsLongLat(data):
     geo_locator = Nominatim(user_agent="Project")
+    latitudes = []
+    longitudes = []
 
     for tweet in data:
         location = tweet.location
-        text = tweet.text
+
         if location is not None:
             # generate latitude and longitude for the given location
             geolocation = geo_locator.geocode(location)
 
             if geolocation is not None:
-                folium.Marker(location=[geolocation.latitude, geolocation.longitude],
-                              popup=text).add_to(plotted_map)
+                latitudes.append(geolocation.latitude)
+                longitudes.append(geolocation.longitude)
+
+    return latitudes, longitudes
+
+
+def createMapMarkers(plotted_map, latitude, longitude):
+    lg = len(longitude)
+
+    for i in range(lg):
+        folium.Marker(location=[latitude[i], longitude[i]]).add_to(plotted_map)
+
+
+def generateLatLongLocations(lat, long):
+    latitude = mean(lat)
+    longitude = mean(long)
+
+    return latitude, longitude
 
 
 def printTweets(list_of_tweets):
@@ -25,13 +43,13 @@ def printTweets(list_of_tweets):
         print("----------------------------------------")
 
 
-def plotMap(hashtag, number):
-    tweets = st.generateTweetsWithHashtag(hashtag, number)
-    printTweets(tweets)
-
+def plotMap(latitudes, longitudes, latitude, longitude):
     # create map
-    plotted = folium.Map(location=[0, 0], zoom_start=3)
+    # location <=> latitude and longitude of the map
+    # tiles <=> how the map looks
+    plotted = folium.Map(location=[latitude, longitude], tiles='cartodb positron',
+                         zoom_start=3, max_zoom=7)
 
     # put markers on the map (the locations of the tweets)
-    createMapMarkers(plotted, tweets)
+    createMapMarkers(plotted, latitudes, longitudes)
     plotted.save("map.html")
