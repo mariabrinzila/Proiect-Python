d4dcff6b25d96893944e58ae19607a77bf723793
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2021-12-21 20:02:59 +0200

    plot_map UPDATE
    createMapMarkers <=> create markers for locations that are not None and that have latitude and longitude generated

--- plot_map.py
+++ plot_map.py
@@ -1,37 +1,16 @@
 import folium
-from geopy.geocoders import Nominatim
 from numpy import mean
 
 
-def locationsLongLat(data):
-    geo_locator = Nominatim(user_agent="Project")
-    latitudes = []
-    longitudes = []
-
-    for tweet in data:
-        location = tweet.location
-
-        if location is not None:
-            # generate latitude and longitude for the given location
-            geolocation = geo_locator.geocode(location)
-
-            if geolocation is not None:
-                latitudes.append(geolocation.latitude)
-                longitudes.append(geolocation.longitude)
-
-    return latitudes, longitudes
+def createMapMarkers(plotted_map, tweets):
+    for tweet in tweets:
+        if tweet.location is not None and tweet.latitude != 0 and tweet.longitude != 0:
+            folium.Marker(location=[tweet.latitude, tweet.longitude], popup=tweet.text).add_to(plotted_map)
 
 
-def createMapMarkers(plotted_map, latitude, longitude):
-    lg = len(longitude)
-
-    for i in range(lg):
-        folium.Marker(location=[latitude[i], longitude[i]]).add_to(plotted_map)
-
-
-def generateLatLongLocations(lat, long):
-    latitude = mean(lat)
-    longitude = mean(long)
+def generateLatLongLocations(locations):
+    latitude = mean([location.latitude for location in locations])
+    longitude = mean([location.longitude for location in locations])
 
     return latitude, longitude
 
@@ -40,10 +19,11 @@ def printTweets(list_of_tweets):
     for t in list_of_tweets:
         print("Tweet text: " + str(t.text))
         print("Tweet location: " + str(t.location))
+        print("Tweet coordinates: " + str(t.latitude) + " and " + str(t.longitude))
         print("----------------------------------------")
 
 
-def plotMap(latitudes, longitudes, latitude, longitude):
+def plotMap(latitude, longitude, tweets):
     # create map
     # location <=> latitude and longitude of the map
     # tiles <=> how the map looks
@@ -51,5 +31,5 @@ def plotMap(latitudes, longitudes, latitude, longitude):
                          zoom_start=3, max_zoom=7)
 
     # put markers on the map (the locations of the tweets)
-    createMapMarkers(plotted, latitudes, longitudes)
+    createMapMarkers(plotted, tweets)
     plotted.save("map.html")
