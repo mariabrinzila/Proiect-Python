952cb44704aaddcb17b29913edfdd9b8a6bed478
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2021-12-27 19:23:20 +0200

    main UPDATE
    created a basic graphic interface

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-import search_tweets as st
-import plot_map as pm
+import tkinter as t
+import get_input_generate_output as gigo
 
-tweets, locations = st.generateTweetsWithHashtag("#covid19", 30)
-pm.printTweets(tweets)
-print("Number of locations on the map: " + str(len(locations)))
+# create GUI
+# create a window
+window = t.Tk()
+window.geometry("750x250")
 
-latitude, longitude = pm.generateLatLongLocations(locations)
-print("Latitude: " + str(latitude) + " and longitude: " + str(longitude))
-pm.plotMap(latitude, longitude, tweets)
+# get hashtag from user
+hashtag_variable = t.StringVar()
+ask_user_hashtag = t.Label(text="What hashtag do the tweets you're looking for have?")
+ask_user_hashtag.pack()
+hashtag_entry = t.Entry(textvariable=hashtag_variable, width=25)
+hashtag_entry.pack()
+
+# get number of tweets from user
+number_variable = t.IntVar()
+ask_user_number = t.Label(text="How many tweets with that hashtag shall we look for?")
+ask_user_number.pack()
+number_entry = t.Entry(textvariable=number_variable, width=25)
+number_entry.pack()
+okay = t.Button(window, text="All good!", command=gigo.submittedInput)
+okay.pack()
+
+# display the window
+window.mainloop()
