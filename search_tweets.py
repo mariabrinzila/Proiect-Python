8cf7074cb6de17b9e82c14b723cf677e812c627a
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2022-01-08 19:07:00 +0200

    search_tweets UPDATE
    reformat code and rename functions according to the PEP documentation

--- search_tweets.py
+++ search_tweets.py
@@ -28,33 +28,38 @@ class Location:
         self.longitude = long
 
 
-def generateTweetsWithHashtag(hashtag, number):
+def generate_tweets_with_hashtag(hashtag, number):
     """
-    Function to connect to the Twitter API, search for the most recent number tweets with the hashtag
-    hashtag, generate the coordinates of the location of each tweet (for the location that have valid
-    locations)
+    Function to connect to the Twitter API, search for the
+    most recent number tweets with the hashtag hashtag, generate
+    the coordinates of the location of each tweet (for the location
+    that have valid locations)
     :param hashtag: hashtag that the user has inputted
     :param number: number that the user has inputted
-    :return: tweets_with_hashtag (list of Tweet objects containing all the tweets generated) and
-    locations (list of Location objects containing the coordinates of each generated tweet's location,
-    if its location is valid)
+    :return: tweets_with_hashtag (list of Tweet objects containing
+    all the tweets generated) and
+    locations (list of Location objects containing the coordinates
+    of each generated tweet's location, if its location is valid)
     """
     tweets_with_hashtag = []
-    API = tc.connectToAPI()
+    api = tc.connect_to_twitter()
     geo_locator = Nominatim(user_agent="Project")
     locations = []
 
-    # search number of tweets with the hashtag posted in the last 30 days
-    # calculate their coordinates
-    # latitude 0 and longitude 0 <=> Gulf of Guinea
-    for tweets in API.search_30_day(label="Recent", query=hashtag, maxResults=number):
+    # Search number of tweets with the hashtag posted in the last 30 days
+    # Calculate their coordinates
+    # Latitude 0 and longitude 0 <=> Gulf of Guinea
+    for tweets in api.search_30_day(label="Recent", query=hashtag,
+                                    maxResults=number):
         if tweets.user.location is not None:
             tweet_coordinates = geo_locator.geocode(tweets.user.location)
 
             if tweet_coordinates is not None:
-                current_tweet = Tweet(tweets.text, tweets.user.location, tweet_coordinates.longitude,
+                current_tweet = Tweet(tweets.text, tweets.user.location,
+                                      tweet_coordinates.longitude,
                                       tweet_coordinates.latitude)
-                current_location = Location(tweet_coordinates.latitude, tweet_coordinates.longitude)
+                current_location = Location(tweet_coordinates.latitude,
+                                            tweet_coordinates.longitude)
                 locations.append(current_location)
             else:
                 current_tweet = Tweet(tweets.text, tweets.user.location, 0, 0)
