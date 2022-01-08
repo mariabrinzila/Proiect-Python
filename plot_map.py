9e37e8166b790149caeeb97e8e8834f394d59485
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2022-01-08 19:09:46 +0200

    plot_map UPDATE
    reformat code and rename functions according to the PEP documentation and open the html file automatically after the user inputs the hashtag and number without errors and clicks on the All good button

--- plot_map.py
+++ plot_map.py
@@ -1,25 +1,32 @@
 import folium
 from numpy import mean
+import webbrowser
 
 
-def createMapMarkers(plotted_map, tweets):
+def create_map_markers(plotted_map, tweets):
     """
-    Function to create map markers according to the locations of the tweets generated
+    Function to create map markers according to the
+    locations of the tweets generated
     :param plotted_map: the map we want to plot
-    :param tweets: list of Tweet objects containing all the tweets generated
+    :param tweets: list of Tweet objects containing all the
+    tweets generated
     :return: void
     """
     for tweet in tweets:
-        if tweet.location is not None and tweet.latitude != 0 and tweet.longitude != 0:
-            folium.Marker(location=[tweet.latitude, tweet.longitude], popup=tweet.text).add_to(plotted_map)
+        if tweet.location is not None and tweet.latitude != 0 \
+                and tweet.longitude != 0:
+            folium.Marker(location=[tweet.latitude, tweet.longitude],
+                          popup=tweet.text).add_to(plotted_map)
 
 
-def generateLatLongLocations(locations):
+def generate_lat_long_locations(locations):
     """
     Function to generate the coordinates of the map we generate it with
-    :param locations: list of Location objects containing the coordinates of each generated tweet's location,
+    :param locations: list of Location objects containing the
+    coordinates of each generated tweet's location,
     if its location is valid
-    :return: latitude (mean of the locations' latitudes) and longitude (mean of the locations' longitudes)
+    :return: latitude (mean of the locations' latitudes)
+    and longitude (mean of the locations' longitudes)
     """
     latitude = mean([location.latitude for location in locations])
     longitude = mean([location.longitude for location in locations])
@@ -27,33 +34,45 @@ def generateLatLongLocations(locations):
     return latitude, longitude
 
 
-def printTweets(list_of_tweets):
+def print_tweets(list_of_tweets):
     """
-    Function to print all tweets generated (their text, location and coordinates)
-    :param list_of_tweets: list of Tweet objects containing all the tweets generated
+    Function to print all tweets generated (their text, location and
+    coordinates)
+    :param list_of_tweets: list of Tweet objects containing all the
+    tweets generated
     :return: void
     """
     for t in list_of_tweets:
         print("Tweet text: " + str(t.text))
         print("Tweet location: " + str(t.location))
-        print("Tweet coordinates: " + str(t.latitude) + " and " + str(t.longitude))
+        print("Tweet coordinates: " + str(t.latitude) + " and " +
+              str(t.longitude))
         print("----------------------------------------")
 
 
-def plotMap(latitude, longitude, tweets):
+def plot_map(latitude, longitude, tweets):
     """
-    Function to plot the generated tweets' locations on a map and save it in a html file
-    :param latitude: latitude coordinate for the location attribute (when we make the map)
-    :param longitude: longitude coordinate for the location attribute (when we make the map)
-    :param tweets: list of Tweet objects containing all the tweets generated
+    Function to plot the generated tweets' locations on a map and
+    save it in a html file and open it in browser
+    :param latitude: latitude coordinate for the location
+    attribute (when we make the map)
+    :param longitude: longitude coordinate for the location
+    attribute (when we make the map)
+    :param tweets: list of Tweet objects containing all the
+    tweets generated
     :return: void
     """
-    # create map
-    # location <=> latitude and longitude of the map
-    # tiles <=> how the map looks
-    plotted = folium.Map(location=[latitude, longitude], tiles='cartodb positron',
+    # Create map
+    # Location <=> latitude and longitude of the map
+    # Tiles <=> how the map looks
+    plotted = folium.Map(location=[latitude, longitude],
+                         tiles='cartodb positron',
                          zoom_start=3, max_zoom=7)
 
     # put markers on the map (the locations of the tweets)
-    createMapMarkers(plotted, tweets)
-    plotted.save("map.html")
+    create_map_markers(plotted, tweets)
+    path = "map.html"
+    plotted.save(path)
+
+    # Open map in browser
+    webbrowser.open(path)
