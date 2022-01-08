e492aa2f7e54b37eb380e7e5fcd88f464b805172
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2022-01-08 19:05:45 +0200

    twitter_connection UPDATE
    reformat code and rename functions according to the PEP documentation

--- twitter_connection.py
+++ twitter_connection.py
@@ -1,19 +1,18 @@
 import tweepy as tw
 
-# app keys
+# App keys
 api_key = 'DTNhUzQ7EHUI9XHQZ9xuUrc2G'
 api_secret = 'JShGSkczHzAMOAY5WZFk6vpSUETsm6XVkaWbaFDIdySTeIZJS5'
 token_key = '1466436088105422863-H7TXzq461PEVPTDDP7a7FHsovrXz7y'
 token_secret = '734ljvDU0Al2NW1WOwiR3NVndigHxRUuYRGcqeDbN06JF'
 
 
-def connectToAPI():
+def connect_to_twitter():
     """
-    Function to connect to the Twitter API with the api key and secret, token key and secret generated in
-    the Twitter developer portal
+    Function to connect to the Twitter API with the api key and secret,
+    token key and secret generated in the Twitter developer portal
     :return: api (the connection to the API)
     """
-    # connecting to Twitter, using the Twitter tweepy API
     auth = tw.OAuthHandler(api_key, api_secret)
     auth.set_access_token(token_key, token_secret)
     auth.secure = True
