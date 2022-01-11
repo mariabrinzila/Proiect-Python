f80fe4e96fd4c6ae1d1385465899008ab898f383
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2022-01-11 17:12:32 +0200

    plot_map UPDATE
    rephrase comments and reformat code according to the PEP documentation

--- plot_map.py
+++ plot_map.py
@@ -21,7 +21,7 @@ def create_map_markers(plotted_map, tweets):
 
 def generate_lat_long_locations(locations):
     """
-    Function to generate the coordinates of the map we generate it with
+    Function to generate the initial coordinates of the map we generate
     :param locations: list of Location objects containing the
     coordinates of each generated tweet's location,
     if its location is valid
@@ -45,14 +45,14 @@ def print_tweets(list_of_tweets):
     for t in list_of_tweets:
         print("Tweet text: " + str(t.text))
         print("Tweet location: " + str(t.location))
-        print("Tweet coordinates: " + str(t.latitude) + " and " +
-              str(t.longitude))
+        print("Tweet coordinates: " + str(t.latitude) + " and "
+              + str(t.longitude))
         print("----------------------------------------")
 
 
 def plot_map(latitude, longitude, tweets):
     """
-    Function to plot the generated tweets' locations on a map and
+    Function to plot the generated tweets' locations on a map,
     save it in a html file and open it in browser
     :param latitude: latitude coordinate for the location
     attribute (when we make the map)
@@ -66,10 +66,10 @@ def plot_map(latitude, longitude, tweets):
     # Location <=> latitude and longitude of the map
     # Tiles <=> how the map looks
     plotted = folium.Map(location=[latitude, longitude],
-                         tiles='cartodb positron',
+                         tiles="cartodb positron",
                          zoom_start=3, max_zoom=7)
 
-    # put markers on the map (the locations of the tweets)
+    # Put markers on the map (the locations of the tweets)
     create_map_markers(plotted, tweets)
     path = "map.html"
     plotted.save(path)
